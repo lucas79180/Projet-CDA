54c92a6e09f6717a3a805043e28862669cdd2f16
MathisDeschamps <mathis.deschamps2023@campus-eni.fr>
2024-04-12 12:23:09 +0200

    souvenir toi

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,96 +1,67 @@
-<script setup>
-// on définie dans le fichier une variable axios qui prend la valeur exportée dans le fichier '@/axios/instance.js'
-import axios from '@/axios/instance'
-import router from '@/router'
-import { ref, onMounted } from 'vue'
-import FormTextElement from "@/components/FormTextElement.vue";
-
-/*
-* on définit les événements qui peuvent être envoyés par le composant
-*/
-const emit = defineEmits(['login'])
-
-/********************************
- *  Initialisation du modèle ****
- *********************************/
-
-// membre : va être utilisé pour remplir notre formulaire
-const user = ref({
-  pseudo : '',
-  mot_de_passe : '',
-})
-
-// errors : va être utilisé pour lister nos erreurs
-const errors = ref()
-
-
-/********************************
- ****** FONCTIONS ****************
- *********************************/
-
-
-/**
- * creermembre() : est appelée lorsque j'appuie sur le bouton Valider du formulaire
- */
-
- async function login(){
-  /*
-  * utilisation d'axios : nécessite de faire un "npm install axios" pour installer la librairie et de faire dans le script : import axios from 'axios'
-  * axios.post(URL, data) va envoyer les données data au format json à l'api d'url : URL
-  * 
-  * membre.value correspond à la valeur de l'objet membre de notre modèle (mis à jour par le formulaire)
-  * 
-  * await : permet d'attendre que la requête ait recuperé la réponse avant de passer à l'instruction suivante
-  */
-  try {
-    const result = await axios.post('/login', user.value)
-    // si tout se passe bien, 
-    // 1 - on met le token recuperé dans localstorage
-    localStorage.setItem('jwt', result.data);
-
-
-    // 2 - on emet un événement 'login' pour que le composant APP recharge la barre de navigation
-    emit('login')
-    // 3 - on redirige sur la page d'accueil
-     await router.push('/')
-    window.location.reload();
-  } catch (error) {
-    // si jamais il y a une erreur => on met à jour le modèle  
-    errors.value = 'erreur de connexion'
-  }
- }
-
-localStorage.setItem('userInfo', JSON.stringify(user.value.pseudo));
-</script>
-
 <template>
   <main>
     <h1>Login</h1>
-
-     <!-- j'affiche la liste des erreurs potentielles renvoyées par la dernière requête http -->
-   <ul class="error" v-if="errors">
-    <li class="error">{{errors}}</li>
-   </ul>
-    <!-- 
-      on garde la balise form pour des raisons sémantiques (meilleure accessibilité, référencement internet)
-      car on va envoyer une requête HTTP via Javascript
-    -->
-  <form>
-        <!-- Pseudo -->
-        <div class="form-element">
-          <label>Pseudo</label>
-          <!-- v-model : équivalent de th:field en vuejs -->
-          <input type="text" v-model="user.pseudo">
-        </div>
-        <!-- Mot de passe -->
-        <div class="form-element">
-          <label>Mot de passe</label>
-          <!-- v-model : équivalent de th:field en vuejs -->
-          <input type="text" v-model="user.mot_de_passe">
-        </div>
-        <!-- Validation (on utilise un type button pour ne pas valider le formulaire)-->
-        <button type="button" @click="login">Valider</button>
+    <ul class="error" v-if="errors">
+      <li class="error">{{errors}}</li>
+    </ul>
+    <form>
+      <div class="form-element">
+        <label>Pseudo</label>
+        <input type="text" v-model="user.pseudo">
+      </div>
+      <div class="form-element">
+        <label>Mot de passe</label>
+        <input type="password" v-model="user.mot_de_passe">
+      </div>
+      <div class="form-element">
+        <label>
+          <input type="checkbox" v-model="user.se_souvenir_de_moi">
+          Se souvenir de moi
+        </label>
+      </div>
+      <button type="button" @click="login">Valider</button>
+      <button type="button" @click="forgotPassword">Mot de passe oublié ?</button>
     </form>
-
   </main>
 </template>
+
+<script setup>
+import axios from '@/axios/instance'
+import router from '@/router'
+import {ref, onMounted} from 'vue'
+import FormTextElement from "@/components/FormTextElement.vue";
+
+const emit = defineEmits(['login'])
+
+const user = ref({
+  pseudo: '',
+  mot_de_passe: '',
+  se_souvenir_de_moi: false,
+})
+
+const errors = ref()
+
+async function login() {
+  try {
+    const result = await axios.post('/login', user.value)
+    localStorage.setItem('jwt', result.data);
+
+    if (user.value.se_souvenir_de_moi) {
+      localStorage.setItem('userInfo', JSON.stringify(user.value));
+    } else {
+      localStorage.removeItem('userInfo');
+    }
+
+    emit('login')
+    await router.push('/')
+    window.location.reload();
+  } catch (error) {
+    errors.value = 'erreur de connexion'
+  }
+}
+
+function forgotPassword() {
+  // Rediriger l'utilisateur vers la page de réinitialisation du mot de passe
+  router.push('/forgot-password');
+}
+</script>
