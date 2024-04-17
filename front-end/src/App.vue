57e46f3cdd9f6696b0d0c3dd3a4ae1027f3f3801
MathisDeschamps <mathis.deschamps2023@campus-eni.fr>
2024-04-17 16:32:09 +0200

    barre de recherche

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,24 +3,7 @@
     <Navbar />
     <div class="content">
       <router-view></router-view>
-      <div v-if="isHomePage" class="filters">
-        <p>Filtres :</p>
-      </div>
-      <div v-if="isHomePage" class="search-container">
-        <input type="text" placeholder="Recherche...">
-      </div>
-      <div v-if="isHomePage" class="filters">
-        <p>Catégories :</p>
-      </div>
-      <div v-if="isHomePage" class="select-container">
-        <select>
-          <option value="">Choisissez une catégorie</option>
-          <option value="informatiques">Informatiques</option>
-          <option value="ameublement">Ameublement</option>
-          <option value="vêtements">Vêtements</option>
-          <option value="sports-loisirs">Sports & Loisirs</option>
-        </select>
-      </div>
+
       <div v-if="isHomePage" class="filters">
         <TablesEncheres />
       </div>
